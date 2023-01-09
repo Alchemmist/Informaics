dc94842765f86683c455a5751b417d07a33112ef
user36672 <anton.ingrish@gmail.com>
2023-01-09 14:11:00 +0300

    commmit

---
+++ Круг.py
@@ -0,0 +1,6 @@
+import math
+
+
+r = int(input())
+print(f'{(math.pi * (r ** 2)):.3f}')
+print(f'{(2 * math.pi * r):.3f}')
--- Три числа.py
+++ Три числа.py
@@ -1,4 +1,4 @@
 a, b, c = map(int, input().split())
 print(f'{a}+{b}+{c}={a + b + c}')
 print(f'{a}*{b}*{c}={a * c * b}')
-print(f'({a}+{b}+{c})/3={((a + b+ c)/3):.2f}')
+print(f'({a}+{b}+{c})/3={((a + b+ c)/3):.3f}')
