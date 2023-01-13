e9a87860ff7cb32c287361fd61787c207dc75e6b
user36672 <anton.ingrish@gmail.com>
2023-01-13 09:14:37 +0300

    commit

--- 57 Ветвления/Новый год.py
+++ 57 Ветвления/Новый год.py
@@ -7,10 +7,9 @@ def get_day_by_month(month_number: int) -> int:
 
 
 month, day = map(int, input().split())
-if month < 0 or month > 12 or day > 31 or day < 0:
+if month < 0 or month > 12 or day > 31 or day < 0 or (month == 2 and day > 28):
     print(-1)
     exit()
-
 diff = 365 - day
 for i in range(1, month):
     diff -= get_day_by_month(i)
