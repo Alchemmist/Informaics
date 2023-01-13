d49fba5cfe4fa04c22efbf62b435c8f7136f939b
user36672 <anton.ingrish@gmail.com>
2023-01-13 08:35:31 +0300

    commit

--- 57 Ветвления/Новый год.py
+++ 57 Ветвления/Новый год.py
@@ -1,3 +1,17 @@
+import calendar
+
+
+def get_day_by_month(month_number: int) -> int:
+    num_days = calendar.monthrange(2023, month_number)[1]
+    return num_days
+
+
 month, day = map(int, input().split())
-m1 = (month - 1)
-print(365 - ((m1 * 31) + (m1 * 30) + day))
+if month < 0 or month > 12 or day > 31 or day < 0:
+    print(-1)
+    exit()
+
+diff = 365 - day
+for i in range(1, month):
+    diff -= get_day_by_month(i)
+print(diff if diff != 0 else '365')
--- 57 Ветвления/Сколько дней в месяце.py
+++ 57 Ветвления/Сколько дней в месяце.py
@@ -1,16 +1,8 @@
-seasons = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 30,
-    9: 31,
-    10: 30,
-    11: 31,
-    12: 30,
+import calendar
 
-}
-exec("try:print(seasons[int(input())])\nexcept KeyError:print('0')")
+
+try:
+    num_days = calendar.monthrange(2023, int(input()))[1]
+    print(num_days)
+except calendar.IllegalMonthError:
+    print(0)
