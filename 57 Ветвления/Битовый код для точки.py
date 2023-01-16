a5f4c40f0a01ac897521b18ff41ed907b7f9734a
user36672 <anton.ingrish@gmail.com>
2023-01-16 14:20:23 +0300

    commit

--- 57 Ветвления/Битовый код для точки.py
+++ 57 Ветвления/Битовый код для точки.py
@@ -1,6 +1,6 @@
 x, y = map(float, input().split())
 
-print(1 if y <= 1 else 0, end='')
-print(1 if y <= -x else 0, end='')
-print(1 if -1 <= y <= 1 and -1 <= x <= 1 else 0, end='')
-print(1 if -1 <= y <= 1 and 0 <= x <= 2 else 0)
+print(1 if y < 1 else 0, end='')
+print(1 if y < -x else 0, end='')
+print(1 if -1 < y < 1 and -1 < x < 1 else 0, end='')
+print(1 if -1 < y < 1 and 0 < x < 2 else 0)
