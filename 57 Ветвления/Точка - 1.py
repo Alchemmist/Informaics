62a773023d388cf61ff74e14238c5dd3d27cd8b4
user36672 <anton.ingrish@gmail.com>
2023-01-16 14:17:26 +0300

    commit

--- 57 Ветвления/Точка - 1.py
+++ 57 Ветвления/Точка - 1.py
@@ -1,7 +1,10 @@
 x, y = map(float, input().split())
-if (x ** 2 + y ** 2 >= 4) and \
-   (y <= x) and \
-   (x <= 2):
+if (x ** 2 + y ** 2 > 4) and \
+   (y < x) and \
+   (x < 2) and \
+    y > 0:
     print('YES')
 else:
     print('NO')
+
+
