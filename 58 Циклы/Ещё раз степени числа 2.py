86ad69778dfae23c6b1e49c638d022bb5d5cbb7d
user36672 <anton.ingrish@gmail.com>
2023-01-30 14:19:49 +0300

    commit

--- 58 Циклы/Ещё раз степени числа 2.py
+++ 58 Циклы/Ещё раз степени числа 2.py
@@ -1,9 +1,11 @@
 n = int(input())
 check = True
-while n > 0:
+s = 0
+while s < n:
     n -= 1
     if n % 2 == 0 and n > 0:
         print(2 ** n, end=' ')
         check = False
 if check:
     print(0)
+
