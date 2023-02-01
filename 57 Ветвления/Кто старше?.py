a00af6fb97198c0be10f06b87a9ed7dd393888d0
Alchemmist <anton.ingrish@gmail.com>
2023-02-01 04:09:35 +0300

    commit

--- 57 Ветвления/Кто старше?.py
+++
@@ -1,2 +0,0 @@
-age = {key: int(value) for value, key in zip(input().split(), ['A', 'B', 'C'])}
-print(*a if len(a := [key for key, value in age.items() if value >= max(age.values())]) < 3 else '0')
--- 58 Циклы/Алгоритм Евклида.py
+++ 58 Циклы/Алгоритм Евклида.py
@@ -1,7 +1,21 @@
 a, b = sorted(list(map(int, input().split())), reverse=True)
 iters = 0
-while a != b:
-    a -= b
-    iters += 1
 
-print(a, iters)
+# O(n // 100)
+# while b != 0: 
+#     a, b = b, a % b 
+#     iters += 1
+# print(a, iters)
+
+
+# O(n)
+while a != 0 and b != 0:
+    if a > b: 
+        a = a - b
+        iters += 1
+    else: 
+        b = b - a 
+        iters += 1
+
+print(max(a, b), iters)
+
