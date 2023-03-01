cb06bc3d547f211acfc79d93509884af026d7812
user36672 <anton.ingrish@gmail.com>
2023-03-01 10:17:14 +0300

    commit

--- 62 Массивы/Соседи с минимальной суммой.py
+++ 62 Массивы/Соседи с минимальной суммой.py
@@ -4,5 +4,12 @@ import random
 a, b, n = map(int, input().split())
 random_nums = [random.randint(a, b) for _ in range(n)]
 print(*random_nums)
-random_nums2 = random_nums.copy()
+last_num = random_nums[0]
+summ = float('inf')
 
+for ind, num in enumerate(random_nums[:-1]):
+    if num + random_nums[ind + 1] <= summ:
+        result = (ind, ind + 1)
+        summ = num + random_nums[ind + 1]
+
+print(result[0] + 1, result[1] + 1)
